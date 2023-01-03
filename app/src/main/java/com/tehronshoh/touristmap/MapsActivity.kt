a8cb1de78eb53d1938b5b92576f22134e104f3a0
Tehronshoh Ahatzoda <tehronkarimov55@mail.ru>
2023-01-03 20:15:36 +0300

    :Map is showing!

--- app/src/main/java/com/tehronshoh/touristmap/MapsActivity.kt
+++ app/src/main/java/com/tehronshoh/touristmap/MapsActivity.kt
@@ -1,8 +1,8 @@
 package com.tehronshoh.touristmap
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -39,9 +39,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Dushanbe and move the camera
+        val dushanbe = LatLng(38.53575, 68.77905)
+        mMap.addMarker(MarkerOptions().position(dushanbe).title("Marker in Dushanbe"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dushanbe))
     }
 }
