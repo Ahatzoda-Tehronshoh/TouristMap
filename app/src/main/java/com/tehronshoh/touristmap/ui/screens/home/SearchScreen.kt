5abd70ae0ac455c1e3c754b2ff09186db2f87649
Tehronshoh Ahatzoda <tehronkarimov55@mail.ru>
2023-06-13 18:55:49 +0500

    create searching TextField

--- app/src/main/java/com/tehronshoh/touristmap/ui/screens/home/SearchScreen.kt
+++ app/src/main/java/com/tehronshoh/touristmap/ui/screens/home/SearchScreen.kt
@@ -1,33 +1,109 @@
 package com.tehronshoh.touristmap.ui.screens.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tehronshoh.touristmap.R
 
 @Composable
 fun SearchScreen() {
+    var searchingName by rememberSaveable { mutableStateOf("") }
+
+    val focusRequester = remember { FocusRequester() }
+    var searchFocusState by rememberSaveable { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = colorResource(id = R.color.white)),
+            .background(color = colorResource(id = R.color.white))
+            .padding(horizontal = 30.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Search Screen",
-            fontSize = 32.sp,
-            style = TextStyle(color = colorResource(id = R.color.primary))
+        TextField(
+            value = searchingName,
+            onValueChange = { searchingName = it },
+            placeholder = {
+                Text(
+                    text = "Введите название",
+                    fontFamily = FontFamily(Font(resId = R.font.montserrat_600)),
+                    fontSize = 12.sp,
+                    style = TextStyle(color = Color.Black, fontWeight = FontWeight.W400),
+                    modifier = Modifier.alpha(0.4f)
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.search_icon),
+                    contentDescription = "email",
+                    tint = colorResource(id = R.color.primary),
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+            ),
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .border(
+                    width = 1.dp,
+                    color = colorResource(id = R.color.black_10_alpha),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .background(color = colorResource(id = R.color.white))
+                .padding(horizontal = 12.dp)
+                .focusRequester(focusRequester)
+                .onFocusChanged { searchFocusState = it.hasFocus }
         )
+
+        SideEffect {
+            if (searchFocusState)
+                focusRequester.requestFocus()
+        }
     }
 }
 
